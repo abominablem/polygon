f0e06df5153ef3fab31dd288b953cf38d2e89c3f
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2022-02-21 23:39:15 +0000

    Created CompletionTracker and placeholder buttons. Implemented WidgetSet.

--- tv_tracker.py
+++ tv_tracker.py
@@ -16,7 +16,7 @@ import tk_arrange as tka
 import described_widgets as dw
 import constants as c
 import base
-from futil import get_tk, format_time
+import futil
 from imdb_functions import imdbf
 from widgets import FlexLabel, Counter, Padding, RangeDisplay, PolygonButton
 from log_entry import LogEntryWindow
@@ -47,10 +47,10 @@ class SeasonCounter(tk.Frame):
         self.font_count_size = 30
         self.font_count = ("Calibri", self.font_count_size)
 
-        self.counter = FlexLabel(
+        self.counter = tk.Label(
             self.counter_frame.inner, text = "Season 1",
             font = self.font_count, padx = 25, image = self._pixel,
-            compound = "center", anchor = "center"
+            compound = "center", anchor = "center", width = 250
             )
 
         self.counter.grid(row = 0, column = 0, **c.GRID_STICKY)
@@ -62,7 +62,7 @@ class SeasonCounter(tk.Frame):
         self.season = int(count)
         count = self.enforce_bounds(count)
         self.counter.config(text = "Season %s" % count)
-        # self.fit_text()
+        self.event_generate("<<SeasonChange>>")
 
     @log_class
     def get(self, *args, **kwargs):
@@ -71,14 +71,14 @@ class SeasonCounter(tk.Frame):
     @log_class
     def set_maximum(self, maximum):
         self.maximum = maximum
-        if self.upper > maximum:
-            self.increment(maximum - self.upper)
+        if self.season > maximum:
+            self.increment(maximum - self.season)
 
     @log_class
     def set_minimum(self, minimum):
         self.minimum = minimum
-        if self.lower < minimum:
-            self.increment(self.lower - minimum)
+        if self.season < minimum:
+            self.increment(self.season - minimum)
 
     @log_class
     def enforce_bounds(self, value):
@@ -102,6 +102,50 @@ class SeasonCounter(tk.Frame):
             self.set(cur_val - 1)
         self.event_generate("<<SeasonChange>>")
 
+class CompletionTracker(base.TrimmedFrame):
+    @log_class
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        columns = {
+            1: {"header": "Season", "width": 110,
+                "stretch": True, "anchor": "center"},
+            2: {"header": "#", "width": 40,
+                "stretch": True, "anchor": "center"},
+            3: {"header": "%", "width": 80,
+                "stretch": True, "anchor": "center"},
+            }
+        self.table = dw.SimpleTreeview(
+            self.inner, columns, style = "EpisodeTable.Treeview", edit = True
+            )
+        self.table.grid(row = 0, column = 0, **c.GRID_STICKY)
+        self.rowconfigure(0, weight = 1)
+        self.columnconfigure(0, weight = 1)
+
+    @log_class
+    def update(self, series):
+        self.table.clear()
+        for number, season in series.seasons.items():
+            self.table.insert(
+                "", "end", iid = number, text = number, values = (
+                    len(season),
+                    self.get_season_completion(season, "percentage")
+                    )
+                )
+
+    @log_class
+    def get_season_completion(self, season, type = "percentage"):
+        ep_count = len(season)
+        watch_count = len([episode for episode in season
+                           if episode.entry.watched])
+        if type == "percentage":
+            completion = watch_count / ep_count
+            return str(int(completion*100)) + "%"
+        elif type == "ratio":
+            return f"{watch_count}/{ep_count}"
+        else:
+            raise ValueError("Invalid completion type")
+
 class WatchEntry:
     @log_class
     def __init__(self, date = None, rating = None):
@@ -122,7 +166,7 @@ class EpisodeTable(base.TrimmedFrame):
                 "stretch": True, "anchor": "w"},
             3: {"header": "Watched?", "width": 200,
                 "stretch": False, "anchor": "center"},
-            4: {"header": "Date watched", "width": 270,
+            4: {"header": "Date watched", "width": 220,
                 "stretch": False, "anchor": "center"},
             5: {"header": "Rating", "width": 150,
                 "stretch": False, "anchor": "center"},
@@ -162,12 +206,9 @@ class EpisodeTable(base.TrimmedFrame):
                     else datetime.strptime(
                         title.entry.date, "%Y-%m-%d").strftime("%d/%m/%Y"))
             rating = "" if title.entry.rating is None else title.entry.rating
-            return (title.title, "Yes", date, rating,
-                    format_time(title.runtime, unit = "minutes"))
+            return (title.title, "Yes", date, rating, title.runtime)
         else:
-            return (title.title, "No", "", "",
-                    format_time(title.runtime, unit = "minutes"))
-
+            return (title.title, "No", "", "", title.runtime)
 
     @log_class
     def clear(self):
@@ -178,93 +219,150 @@ class TvTracker(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         self._during_startup = True
         super().__init__(master, *args, **kwargs)
-        self.window = get_tk(self)
+        self.window = futil.get_tk(self)
 
         """ Build header with buttons and summary """
         self._pixel = tk.PhotoImage(width = 1, height = 1)
-        self.header_frame = tk.Frame(self, bg = c.COLOUR_TV_BACKGROUND)
-        self.show_title_frame = base.TrimmedFrame(self.header_frame)
+        self.widget_frame = tk.Frame(self, bg = c.COLOUR_TV_BACKGROUND)
+        self.show_title_frame = base.TrimmedFrame(self.widget_frame, height = 100)
         self.show_title = tk.Label(
             self.show_title_frame.inner, font = ("Helvetica", 46), padx = 10,
             text = "placeholder text", width = 1000, image = self._pixel,
-            compound = "left", anchor = "w"
+            compound = "left", anchor = "w", height = 100
             )
         self.show_title.grid(row = 0, column = 0, **c.GRID_STICKY)
         self.show_title_frame.columnconfigure(0, weight = 1)
         self.show_title_frame.rowconfigure(0, weight = 1)
 
         self.btn_add_new = PolygonButton(
-            self.header_frame, text = "⠀+⠀", command = self.add_new,
+            self.widget_frame, text = "+", command = self.add_new,
             width = 130, anchor = "center", font = ("Calibri", 45),
+            height = 100
             )
         self.season_display = SeasonCounter(
-            self.header_frame, bg = c.COLOUR_TV_BACKGROUND, width = 200
+            self.widget_frame, bg = c.COLOUR_TV_BACKGROUND, width = 200,
+            height = 100
             )
         self.season_display.bind("<<SeasonChange>>", self.update_table)
 
         self.btn_increment = PolygonButton(
-            self.header_frame, text = "+", command = self.increment,
+            self.widget_frame, text = "+", command = futil.null_function,
             width = 40, anchor = "center", repeatinterval = 100,
             repeatdelay = 500, height = 15
             )
+        self.btn_increment.bind("<1>", self.increment)
         self.btn_increment.bind("<Shift-1>", self.increment_all)
         self.btn_deincrement = PolygonButton(
-            self.header_frame, text = "-", command = self.decrement,
+            self.widget_frame, text = "-", command = futil.null_function,
             width = 40, anchor = "center", repeatinterval = 100,
             repeatdelay = 500, height = 15
             )
+        self.btn_deincrement.bind("<1>", self.decrement)
         self.btn_deincrement.bind("<Shift-1>", self.decrement_all)
 
-        widgets = {
-            1: {'widget': self.show_title_frame,
-                'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 30)},
-                'stretch_width': True, 'stretch_width_weight': 5},
-            2: {'widget': self.btn_add_new,
-                'grid_kwargs': {**c.GRID_STICKY, "pady": 10}},
-            3: {'widget': self.season_display,
-                'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 1)},
-                'stretch_width': True, 'stretch_width_weight': 1},
-            4: {'widget': self.btn_increment,
-                'grid_kwargs': {**c.GRID_STICKY, "padx": 2}},
-            5: {'widget': self.btn_deincrement,
-                'grid_kwargs': {**c.GRID_STICKY, "padx": 2}}
-            }
+        self.episode_table = EpisodeTable(self.widget_frame)
 
-        self.header = tka.WidgetSet(self.header_frame, widgets,
-                                    layout = [[1, 2, 3, 4], [1, 2, 3, 5]])
-        self.header.grid(row = 0, column = 0, **c.GRID_STICKY,
-                          pady = (10, 20), padx = (20, 50))
+        self.btn_save = PolygonButton(
+            self.widget_frame, text = "💾", command = self._click_save,
+            width = 130, anchor = "center", font = ("Calibri", 30)
+            )
+        self.btn_discard = PolygonButton(
+            self.widget_frame, text = "🗑", command = self._click_discard,
+            width = 130, anchor = "center", font = ("Calibri", 30),
+            )
+        self.btn_refresh = PolygonButton(
+            self.widget_frame, text = "⟳", command = self._click_refresh,
+            width = 130, anchor = "center", font = ("Calibri", 30),
+            )
+        self.btn_download = PolygonButton(
+            self.widget_frame, text = "", command = self._click_download,
+            width = 130, anchor = "center", font = ("Calibri", 30),
+            )
+        self.btn_search = PolygonButton(
+            self.widget_frame, text = "🔎", command = self._click_search,
+            width = 130, anchor = "center", font = ("Calibri", 30),
+            )
+        self.btn_random = PolygonButton(
+            self.widget_frame, text = "", command = self._click_random,
+            width = 130, anchor = "center", font = ("Calibri", 30),
+            )
+        self.btn_further_details = PolygonButton(
+            self.widget_frame, text = "xx",
+            command = self._click_further_details, width = 130,
+            anchor = "center", font = ("Calibri", 30),
+            )
 
-        self.body_frame = tk.Frame(self, bg = c.COLOUR_TV_BACKGROUND)
-        self.episode_table = EpisodeTable(self.body_frame)
+        self.completion_tracker = CompletionTracker(
+            self.widget_frame, width = 240
+            )
 
         widgets = {
             1: {'widget': self.episode_table,
                 'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 30)},
                 'stretch_width': True, 'stretch_width_weight': 5,
                 'stretch_height': True},
-            # 2: {'widget': self.btn_add_new,
-            #     'grid_kwargs': {**c.GRID_STICKY, "pady": 10}},
-            # 3: {'widget': self.season_display,
-            #     'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 1)},
-            #     'stretch_width': True, 'stretch_width_weight': 1},
-            # 4: {'widget': self.btn_increment,
-            #     'grid_kwargs': {**c.GRID_STICKY, "padx": 2}},
-            # 5: {'widget': self.btn_deincrement,
-            #     'grid_kwargs': {**c.GRID_STICKY, "padx": 2}}
+            2: {'widget': self.btn_save,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": 1, "padx": 20}},
+            3: {'widget': self.btn_discard,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": (1, 70), "padx": 20}},
+            4: {'widget': self.btn_refresh,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": 1, "padx": 20}},
+            5: {'widget': self.btn_download,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": (1, 70), "padx": 20}},
+            6: {'widget': self.btn_search,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": 1, "padx": 20}},
+            7: {'widget': self.btn_random,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": 1, "padx": 20}},
+            8: {'widget': self.btn_further_details,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": 1, "padx": 20}},
+            9: {'widget': self.completion_tracker,
+                'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 30)},
+                'stretch_width': True, 'stretch_width_weight': 1,
+                'stretch_height': True},
+            10: {'widget': self.show_title_frame,
+                'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 30), "pady": (10, 30)},
+                'stretch_width': True, 'stretch_width_weight': 5},
+            11: {'widget': self.btn_add_new,
+                'grid_kwargs': {**c.GRID_STICKY, "pady": (20, 40), "padx": 20}},
+            12: {'widget': self.season_display,
+                'grid_kwargs': {**c.GRID_STICKY, "padx": (30, 1), "pady": (10, 30)},
+                'stretch_width': True, 'stretch_width_weight': 0},
+            13: {'widget': self.btn_increment,
+                'grid_kwargs': {**c.GRID_STICKY, "padx": 2, "pady": (10, 0)}},
+            14: {'widget': self.btn_deincrement,
+                'grid_kwargs': {**c.GRID_STICKY, "padx": 2, "pady": (0, 30)}},
+            -1: {'widget_kwargs': {"bg": c.COLOUR_TV_BACKGROUND},
+                 'stretch_height': True, 'stretch_width': True, }
             }
-        self.body = tka.WidgetSet(self.body_frame, widgets,
-                                  layout = [[1], [1]])
-        self.body.grid(row = 1, column = 0, **c.GRID_STICKY, padx = (20, 20))
+        self.widget_set = tka.WidgetSet(
+            self.widget_frame, widgets,
+            layout = [[10, 11, 12, 13],
+                      [10, 11, 12, 14],
+                      [1, 2, 9, 9],
+                      [1, 3, 9, 9],
+                      [1, 4, 9, 9],
+                      [1, 5, 9, 9],
+                      [1, 6, 9, 9],
+                      [1, 7, 9, 9],
+                      [1, 8, 9, 9],
+                      [1, -1, 9, 9]]
+            )
+        self.widget_set.grid(row = 0, column = 0, **c.GRID_STICKY, padx = (20, 40))
 
         self.columnconfigure(0, weight = 1)
         self.rowconfigure(1, weight = 1)
 
+        self.series = None
         self.title_id = self.get_startup_title_id()
         season = self.get_first_incomplete_series(self.title_id)
         self.season_display.set(season)
 
         self.update_table()
+
+    @log_class
+    def log_entry(self, event = None):
+        return
+        title_id = self.film_request.get_value()
 
     @log_class
     def dim(self, transparency = 0.5):
@@ -275,37 +373,64 @@ class TvTracker(tk.Frame):
         self.dim(transparency = 1)
 
     @log_class
+    def add_entry(self, event = None):
+        entry_dict = self.log_entry_window.get_dict()
+        entry_dict["title_id"] = self.log_entry_window.title_id
+        self.log_entry_window.destroy()
+        self.undim()
+        imdbf.add_entry(**entry_dict)
+        self.query_data()
+        self.set_counter_range()
+
+    @log_class
     def add_new(self, *args, **kwargs):
-        pass
+        pass #TODO open film request window
+        # self.log_entry()
 
     @log_class
     def increment(self, *args, **kwargs):
-        pass
+        self.season_display.increment()
 
     @log_class
     def decrement(self, *args, **kwargs):
-        pass
+        self.season_display.decrement()
 
     @log_class
     def increment_all(self, *args, **kwargs):
-        pass
+        self.season_display.set(self.season_display.maximum)
 
     @log_class
     def decrement_all(self, *args, **kwargs):
-        pass
+        self.season_display.set(self.season_display.minimum)
 
     @log_class
     def update_table(self, *args, **kwargs):
+        self.episode_table.clear()
         title_id = self.title_id
         season = self.season_display.get()
-        entries = self.get_entries(title_id, season)
-        self.episode_table.add_episodes(entries)
+        series = self.get_series(title_id)
+
+        self.season_display.set_maximum(max(series.seasons))
+        self.season_display.set_minimum(min(series.seasons))
+
+        self.show_title.config(text = series.title)
+        self.completion_tracker.update(series)
+        episodes = series.seasons[season]
+
+        self.episode_table.add_episodes(episodes)
 
     @log_class
-    def get_entries(self, title_id, season):
-        self.episode_table.clear()
-        entries = imdbf.get_series_with_entries(title_id).seasons[season]
-        return entries
+    def get_series(self, title_id, refresh = False):
+        if refresh or self.series is None:
+            series = imdbf.get_series_with_entries(title_id, refresh)
+            if 'unknown' in series.seasons:
+                series.seasons[0] = series.seasons['unknown']
+                del series.seasons['unknown']
+            return series
+        elif title_id != series.title_id:
+            return self.get_series(title_id, refresh = True)
+        else:
+            return self.series
 
     @log_class
     def get_startup_title_id(self):
@@ -320,6 +445,7 @@ class TvTracker(tk.Frame):
         result = base.polygon_db.episodes.select(query)
         return result[0][0]
 
+    @log_class
     def get_first_incomplete_series(self, series_id):
         query = """ SELECT MIN(season) FROM episodes ep
                     LEFT JOIN entries e
@@ -331,6 +457,34 @@ class TvTracker(tk.Frame):
             return 1
         else:
             return int(result)
+
+    @log_class
+    def _click_save(self, event = None):
+        pass
+
+    @log_class
+    def _click_discard(self, event = None):
+        pass
+
+    @log_class
+    def _click_refresh(self, event = None):
+        pass
+
+    @log_class
+    def _click_download(self, event = None):
+        pass
+
+    @log_class
+    def _click_search(self, event = None):
+        pass
+
+    @log_class
+    def _click_random(self, event = None):
+        pass
+
+    @log_class
+    def _click_further_details(self, event = None):
+        pass
 
 
 if __name__ == "__main__":
