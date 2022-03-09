21f92de9cf107c2d5319435e0ae3f9ffa1ad5614
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2022-03-09 23:40:46 +0000

    Added icons to switch between tv/film tracking

--- root.py
+++ root.py
@@ -28,10 +28,25 @@ class Polygon(tk.Tk):
         self.protocol("WM_DELETE_WINDOW", self.destroy)
         self.columnconfigure(0, weight = 1)
         self.rowconfigure(0, weight = 1)
-        self.style(self)
+        self._style(self)
 
-        self.widget_frame = tk.Frame(self, bg = c.COLOUR_BACKGROUND)
+        self.widget_frame = tk.Frame(self, bg = c.COLOUR_TITLEBAR)
         self.header = base.PolygonFrameBase(self.widget_frame)
+        self.icons = base.IconSet(self.widget_frame, bg = c.COLOUR_TITLEBAR)
+        self.icons.rowconfigure(0, weight = 1)
+        icon_kwargs = dict(
+            bg = c.COLOUR_TITLEBAR, font = ("Calibri", 50),
+            padx = 20, anchor = "center", fg = c.COLOUR_TITLEBAR_ICON,
+            cursor = "hand2", hover = c.COLOUR_TITLEBAR_ICON_HOVER,
+            select = c.COLOUR_TITLEBAR_ICON_SELECTED
+            )
+        self.icons.add(text = "📺", name = "tv", **icon_kwargs)
+        self.icons.add(text = "🎞", name = "film", **icon_kwargs)
+        for name in ["film", "tv"]:
+            self.icons[name].bind(
+                "<1>", lambda event: self.launch_module(event.widget.name),
+                add = "+"
+                )
 
         self.divider = tk.Frame(self.widget_frame, bg = "white", height = 1)
 
@@ -51,16 +66,23 @@ class Polygon(tk.Tk):
                    3: {'widget': self.film_tracker,
                        'grid_kwargs': c.GRID_STICKY,
                        'stretch_width': True, 'stretch_height': True},
+                   4: {'widget': self.icons,
+                       'grid_kwargs': {**c.GRID_STICKY, "padx": (0, 30)}},
                    }
         self.widget_set = tka.WidgetSet(
-            self.widget_frame, widgets, layout = [[1], [2], [3]])
+            self.widget_frame, widgets, layout = [[1, 4], [2], [3]])
 
         self.widget_set.grid(row = 0, column = 0, **c.GRID_STICKY)
         self.launch_module("film")
 
     @log_class
     def launch_module(self, module):
-        widget = {"film": self.film_tracker, "tv": self.tv_tracker}[module]
+        widgets = {"film": self.film_tracker, "tv": self.tv_tracker}
+
+        widget = widgets[module]
+        for name, wdgt in widgets.items():
+            if name != module:
+                wdgt.grid_forget()
         widget.grid(**self.widget_set._get_grid_refs(3), **c.GRID_STICKY)
 
     @log_class
@@ -76,24 +98,61 @@ class Polygon(tk.Tk):
         self.mainloop()
 
     @log_class
-    def style(self, master):
-        style = ttk.Style(master)
+    def _style(self, master):
+        self.style = ttk.Style(master)
 
-        style.layout('TNotebook.Tab', False)
         " TV Tracker - Episode Table "
-        style.configure(
+        self.style.configure(
             "EpisodeTable.Treeview", fieldbackground="white",
             font = ('Calibri', 20), rowheight = 40, relief = 'flat'
             )
-        style.map(
+        self.style.map(
             'EpisodeTable.Treeview', background=[('selected', '#F5F5F5')],
             foreground=[('selected', 'black')]
             )
-        style.configure(
+        self.style.configure(
             "EpisodeTable.Treeview.Heading", font = ('Calibri', 20,'bold'),
             padding = 5, rowheight = 60
             )
 
+        self.style.configure(
+            "SeriesList.Treeview.Heading", font = ('Calibri', 15,'bold'),
+            padding = 5, rowheight = 30
+            )
+        self.style.configure(
+            "SeriesList.Treeview", font = ('Calibri', 12),
+            padding = 5, rowheight = 20
+            )
+
+        " TV Tracker - DownloadData Table "
+        self.style.configure(
+            "DownloadData.Treeview", fieldbackground="white",
+            font = ('Calibri', 20), rowheight = 40, relief = 'flat'
+            )
+        self.style.map(
+            'DownloadData.Treeview',
+            background=[('selected', 'midnight blue')],
+            foreground=[('selected', 'white')]
+            )
+        self.style.configure(
+            "DownloadData.Treeview.Heading", font = ('Calibri', 20,'bold'),
+            padding = 5, rowheight = 60
+            )
+
+        self.style.layout(
+            'PolygonProgress.Horizontal.TProgressbar',
+            [('Horizontal.Progressbar.trough',
+              {'children': [('Horizontal.Progressbar.pbar',
+                              {'side': 'left', 'sticky': 'ns'})],
+                'sticky': 'nswe'}),
+              ('Horizontal.Progressbar.label', {'sticky': ''})])
+        self.style.configure(
+            "PolygonProgress.Horizontal.TProgressbar",
+            foreground = "black", background = "midnight blue",
+            bordercolor = "midnight blue", lightcolor = "midnight blue",
+            darkcolor = "midnight blue", text = '0%',
+            font = ('Calibri', 15, 'bold')
+            )
 
 
 polygon = Polygon()
