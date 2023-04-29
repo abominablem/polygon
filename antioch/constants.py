c15954f6c80a7e461130aacc1c9ee90ebde4cb27
Marcus Hickman <88216015+abominablem@users.noreply.github.com>
2023-04-29 22:39:03 +0100

    Constants for Antioch in Polygon

---
+++ antioch/constants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 18:03:39 2023
+
+@author: marcu
+"""
+
+""" GLOBAL VARS """
+LOG_LEVEL = "min"
+DEBUG = False
+DB_PATH = r'.\sqlite_db\transactions_test.db'
+
+
+""" CLASSIFICATION CONSTS """
+CLASSIFICATION_ID_UNCLASSIFIED = 1
